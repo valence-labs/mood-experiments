c51051d5dca277ffc9d59591f3bac926eb085da5
Julien St-Laurent <julien.stl@valencelabs.com>
2025-04-14 15:30:03 -0400

    chore: Update bucket path

--- mood/constants.py
+++ mood/constants.py
@@ -10,12 +10,12 @@ CACHE_DIR = dm.fs.get_cache_dir("MOOD")
 For the downstream applications (optimization and virtual screening)
 we save all related data to this directory
 """
-DOWNSTREAM_APPS_DATA_DIR = "gs://mood-data-public/downstream_applications/"
+DOWNSTREAM_APPS_DATA_DIR = "https://storage.valencelabs.com/mood-data/downstream_applications/"
 
 """
 Where the results of MOOD are saved
 """
-RESULTS_DIR = "gs://mood-data-public/results/"
+RESULTS_DIR = "https://storage.valencelabs.com/mood-data/results/"
 
 """
 The two downstream applications we consider for MOOD as application areas of molecular scoring
@@ -25,7 +25,7 @@ SUPPORTED_DOWNSTREAM_APPS = ["virtual_screening", "optimization"]
 """
 Where data related to specific datasets is saved
 """
-DATASET_DATA_DIR = "gs://mood-data-public/datasets/"
+DATASET_DATA_DIR = "https://storage.valencelabs.com/mood-data/datasets/"
 
 
 """The number of epochs to train NNs for"""
